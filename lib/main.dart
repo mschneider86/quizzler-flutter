e2ed16d1084e84774445b2cabf5a757705dd7460
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-28 14:30:35 -0300

    added: scorekeeper to modify the score of the user using a list of icons

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class Quizzler extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: QuizPage(),
           ),
-          
         ),
       ),
     );
@@ -26,6 +25,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -63,6 +73,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(Icons.check, color: Colors.green),
+                  );
+                });
               },
             ),
           ),
@@ -81,11 +96,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(Icons.close, color: Colors.red),
+                  );
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scoreKeeper)
       ],
     );
   }
