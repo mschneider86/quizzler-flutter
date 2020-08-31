b2b59cf3a3672b6899ff6340343ad1467e17a491
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-31 11:46:27 -0300

    added: class with the questions and its answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,10 +28,10 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+  Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+  Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+  Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   int questionNumber = 0;
@@ -47,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +73,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer) {
+
+                } else {
+
+                }
+
                 setState(() {
                   questionNumber++;
                 });
